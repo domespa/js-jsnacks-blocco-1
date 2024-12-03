2469e351bc29f44d6208b9fb8efdea3f17a94c42
domespa <dumiii1988@gmail.com>
2024-12-03 11:35:25 +0100

    feat: ex done

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+let number1 = prompt("inserisci il numero 1")
+let number2 = prompt("inserisci il numero 2")
+ 
+if (number1 > number2) {
+    alert("Il numero maggiore è " + number1)
+} else if (number2 > number1) {
+    alert("Il numero maggiore è " + number2)
+} else {
+    alert("I numeri sono uguali")
+}
+
